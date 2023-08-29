04973a4e43348f418b8568f9d9876f936cae80e6
willywillywill <43361233asd@gmail.com>
2023-08-29 15:34:53 +0800

    OK

--- test.py
+++ test.py
@@ -3,92 +3,41 @@ import matplotlib.pylab as plt
 import matplotlib.animation as animation
 import pprint
 
+class Node_state:
+    ball = "ball"
+    car = "car"
+    no = "no"
+    state_dit = { ball:2, car:1, no:0 }
+
+    Node_size = 10
 
 
-class Node:
-    def __init__(self, xy):
-        self.data = np.random.randint(2, size=(10,10))
-        self.xy = xy
-        self.next = None
-        self.node_state_dit = {"car":1, "no":0}
-        self.node_state = self.node_state_dit["no"]
-    def set_state(self, state):
-        self.node_state = self.node_state_dit[state]
-    def set_data(self, x,y, data):
-        self.data[x][y] = data
-    def set_next(self, next):
-        self.next = next
-    def return_state(self):
-        return self.node_state
-    def return_data(self):
-        return self.data
-    def return_xy(self):
-        return self.xy
-
-class Map:
-    def __init__(self,Msize):
-        self.node = [ [Node((i,j)) for j in range(Msize)] for i in range(Msize)]
-        self.next = []
+class Next:  # stack
+    def __init__(self):
+        self.data = []
+    def add_data(self, data):
+        self.data.append(data)
+    def pop_data(self):
+        return self.data.pop(-1)
+    
+class Map_node(Node_state, Next):
+    def __init__(self, node, size):
+        self.size = size
         self.xy = (0,0)
-        self.size = Msize
-    def return_data(self, x,y):
-        return self.node[x][y].return_data()
-    def return_datas(self):
-        data = [
-            [self.return_data(i, j) for j in range(self.size)]
-            for i in range(self.size)
-        ]
-        return data
-    def return_node_state(self,x,y):
-        return  self.node[x][y].return_state()
-    def return_nodes_state(self):
-        data = [
-                [ self.return_node_state(i,j) for j in range(self.size) ]
-                for i in range(self.size)
-                ]
-        return np.array(data)
-    def add_next(self, x,y):
-        self.next.append((x,y))
-    def updata(self):
-        x,y = self.next.pop(0)
-        self.node[self.xy[0]][self.xy[1]].set_next(self.node[x][y])
-        self.node[self.xy[0]][self.xy[1]].set_state("no")
-        self.node[x][y].set_state("car")
-        self.xy = (x,y)
-    def A(self, i):
-        y = i // self.size
-        x = abs(((self.size - 1) * (y % 2)) - (i % self.size))
-        self.add_next(x,y)
-        self.updata()
-        return x,y
+        self.arr = [ [ node for i in range(self.size)] for j in range(self.size) ]
+        
+        self.next = Next()
+    def updata(self, xy=None):
+        if xy != None:
+            self.next.add_data((xy[0],xy[1]))
+        x,y = self.xy
+        self.arr[x][y] = Node_state.state_dit[Node_state.no]
+        
+        x,y =  self.next.pop_data()
+        self.arr[x][y] = Node_state.state_dit[Node_state.car]
+    def return_data(self):
+        return self.arr
 
 
-Msize = 5
-root = Map(Msize)
-fig, ax = plt.subplots(1,2)
-
-root.A(0)
-im1 = ax[0].imshow(root.return_data(0,0))
-im2 = ax[1].imshow(root.return_nodes_state())
-
-def animate(i):
-    ax[0].title.set_text("in Map-%d" % (i))
-    ax[1].title.set_text("MAP-%d" % (i))
-
-    x,y = root.A(i)
-    j = root.return_nodes_state()
-    k = root.return_data(x,y)
-
-    im1.set_data(k)
-    im2.set_data(j)
-
-    return im1
-
-d = [ i for i in range(Msize*Msize) ]
-ani = animation.FuncAnimation(fig,
-                              animate,
-                              frames=d+d[::-1],
-                              interval=200)
-
-
-
+ns = Node_state()
+m = Map_node(ns.state_dit["no"],ns.Node_size)
--- test2.py
+++ test2.py
@@ -18,6 +18,7 @@ class Next:  # stack
         self.data.append(data)
     def pop_data(self):
         return self.data.pop(-1)
+
 class Node(Node_state, Next):
     def __init__(self):
         self.xy = (0, 0)
@@ -25,7 +26,9 @@ class Node(Node_state, Next):
                              for i in range(Node_state.Node_size) ]
                            for j in range(Node_state.Node_size) ]
         self.next = Next()
-    def updata(self): # !
+    def updata(self, xy=None): # !
+        if xy!=None:
+            self.next.add_data((xy[0],xy[1]))
         x,y = self.xy
         self.state_arr[x][y] = Node_state.state_dit[Node_state.no]
 
@@ -34,30 +37,60 @@ class Node(Node_state, Next):
         self.xy = (x,y)
     def return_state(self):
         return self.state_arr
+    
 class Map(Next):
     def __init__(self, Msize):
         self.size = Msize
         self.xy = (0,0)
-        self.map = [ [ [Node, 0 if self.xy != (i,j) else 1] for i in range(self.size) ] for j in range(self.size) ]
+        self.map = [ [ Node() for i in range(self.size) ] for j in range(self.size) ]
         self.next = Next()
-    def updata(self):
-        x,y = self.xy
-        self.map[x][y][1] = 0
+    def updata_map(self, xy=None):
+        if xy!=None:
+            self.next.add_data((xy[0],xy[1]))
 
         x,y = self.next.pop_data()
-        self.map[x][y][1] = 1
         self.xy = (x,y)
-    def return_map(self):
+    def update_node(self, xy=None):
+        x,y = self.xy
+        self.map[x][y].updata(xy)
+    def return_node(self):  # in node 
         x,y = self.xy
         return self.map[x][y].return_state()
-
+    def return_map(self): # in map
+        out = [ [ 1 if self.xy==(i,j) else 0 for j in range(self.size) ] for i in range(self.size) ]
+        return out
 
 
 ns = Node_state()
 Msize = 5
-
-map = Map(Msize)
+m = Map(Msize)
 
 fig, ax = plt.subplots(1,2)
+im1 = ax[0].imshow(m.return_map())
+im2 = ax[1].imshow(m.return_node())
 
+def animate(i):
+    ax[0].title.set_text("map-%d" % (i))
+    ax[1].title.set_text("node-%d" % (i))
 
+    y = i // Msize
+    x = abs(((Msize - 1) * (y % 2)) - (i % Msize))
+
+    m.updata_map((x,y))
+    
+
+    im1_data = m.return_map()
+    im2_data = m.return_node()
+    
+    im1.set_data(im1_data)
+    im2.set_data(im2_data)
+
+    return im1
+
+d = [ i for i in range(Msize*Msize) ]
+ani = animation.FuncAnimation(fig,
+                              animate,
+                              frames=d+d[::-1],
+                              interval=200)
+
+plt.show()
